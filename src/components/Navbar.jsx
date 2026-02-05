b76df6d0b92823147f0976ea79ed444d1191a046
sterlker <hazimqayyim1102@gmail.com>
2026-02-05 16:08:03 +0800

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
     <nav className='bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center'>
       <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex items-center justify-end w-full'>
-          <Link to="/" className='flex items-center gap-2.5'>
+          <Link to="/sterl-portfolio/" className='flex items-center gap-2.5'>
             <Rat className='size-9 text-primary' />
             <span className='hidden lg:block text-3xl font-bold font-mono text-white'>
               Sterlzim7506
@@ -21,19 +21,19 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
           <Menu className='w-7 h-5 relative cursor-pointer z-40 md:hidden' onClick={() => setMenuOpen((prev) => !prev)} />
 
           <div className='hidden md:flex items-center gap-3 sm:gap-4 ml-auto space-x-8'>
-            <Link to={"/about"}>
+            <Link to={"/sterl-portfolio/about"}>
               <span className='text-xl font-mono text-white'>
                 About
               </span>
             </Link>
 
-            <Link to={"/projects"}>
+            <Link to={"/sterl-portfolio/projects"}>
               <span className='text-xl font-mono text-white'>
                 Projects
               </span>
             </Link>
 
-            <Link to={"/contacts"}>
+            <Link to={"/sterl-portfolio/contacts"}>
               <span className='text-xl font-mono text-white'>
                 Contacts
               </span>
